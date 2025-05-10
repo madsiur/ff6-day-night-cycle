6bcb9f1b964f2ac83ed1a96470c6943c2f282abb
madsiur <madsiur@squaresoft.com>
2025-05-10 02:02:14 +0000

    Add hack constants

--- asm/var/global.asm
+++ asm/var/global.asm
@@ -19,3 +19,58 @@ constant code_bank($F20000)
 
 variable end_hack(0)                           // variable used to set next asm
                                                // file starting offset
+
+//-------------------------------------------------------------------------------
+// SRAM constants
+//-------------------------------------------------------------------------------
+// You need 3 bytes of SRAM for this hack: two bytes for the timer and one for
+// its flags. You also need one event bit that will be set when the game consider
+// being at nighttime.
+
+// Some possibilities of SRAM to use:
+// $7E1CF8-$7E1D27: Unused SRAM (FF6j SwTech names)
+// $7E1E1D-$7E1E3F: Unused SRAM
+// $7E1E70-$7E1E7F: Unused SRAM
+
+constant timer($7E1E3D)         // Timer register ($7E1E3D-$7E1E3E)
+
+constant flags($7E1E3F)         // Timer flags
+                                // 0x80 = Progression toward nighttime (automatically toggled)
+                                // 0x40 = Timer is disabled (from event command)
+                                // 0x20 = Player is currently on a regular map (automatically set on regular maps)
+                                // 0x10 = Initial tint is disabled (from event command)
+                                // 0x01-0x0F = Numbers of transitions toward day or night
+
+constant event_byte($7E1E80)    // Night event bit byte ($1E80:2)
+
+constant event_bit($04)         // Event bit
+
+//-------------------------------------------------------------------------------
+// Cycle constants
+//-------------------------------------------------------------------------------
+
+constant transitions(9)         // Number of transitions (0 to 15). Real amount 
+                                // is transitions minus 1. 15 is the limit setting
+                                // but would make nighttime look black and blue only.
+                                // Using a value below 4 would make nighttime and
+                                // daytime look quite similar.
+
+constant init_tint(0)           // init_flags bits (0 or 1)
+constant regular_map(0)         // regular_map and night_prog should never be set
+constant timer_disabled(0)      // manually to 1 other than for testing purpose.
+constant night_prog(1)
+
+constant trans_length($0040)    // Transition length time ($003C = one second)
+
+constant day_length($0400)      // Daytime/nghttime length ($0E10 = one minute)
+
+constant night_value($05)       // Transition ID where a current transition lower
+                                // than this number will result in a nighttime.
+                                // As an example, if this setting is set to 5, 
+                                // transition 0 to 4 will result in nighttime
+
+constant init_flags((night_prog << 7) + (timer_disabled << 6) + (regular_map << 5) + (init_tint << 4) + transitions)
+
+
+
+
